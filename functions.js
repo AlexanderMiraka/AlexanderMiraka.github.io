deb8e3562f1fff1b43cfa20553f1148f39702908
Alexander Miraka <61187586+AlexanderMiraka@users.noreply.github.com>
2026-01-20 13:09:53 +0200

    Fix Carousel slide touch event for mobile

--- functions.js
+++ functions.js
@@ -3,12 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
   navigateButton("personalInfoButton", "workExperience");
   navigateButton("personalProjectsButton", "personalProjects");
   showWorkInfo();
-  slideCarousel();
   closeNavBar();
   openNavBar();
-  navigateButton('aboutBtn','personalInfo');
-  navigateButton('workBtn','workExperience');
-  navigateButton('personalProjectsBtn','personalProjects');
+  navigateButton("aboutBtn", "personalInfo");
+  navigateButton("workBtn", "workExperience");
+  navigateButton("personalProjectsBtn", "personalProjects");
   animatePersonalXp();
 });
 
@@ -29,16 +28,16 @@ function changeTextAnimation() {
 
 function navigateButton(source, destination) {
   const navButton = document.getElementById(source);
-  const slideBar = document.querySelector('.sideBar');
+  const slideBar = document.querySelector(".sideBar");
   const windowWidth = window.innerWidth;
   navButton.addEventListener("click", (e) => {
     e.preventDefault();
     const to = document.getElementById(destination);
     const top = to.offsetTop;
     window.scrollTo({ top: top, behavior: "smooth" });
-    slideBar.classList.remove('open');
-    if(windowWidth < 1024) {
-      document.querySelector('body').style.overflow = '';
+    slideBar.classList.remove("open");
+    if (windowWidth < 1024) {
+      document.querySelector("body").style.overflow = "";
     }
   });
 }
@@ -56,117 +55,168 @@ function showWorkInfo() {
     });
   }
 }
-
-function createCarouselDots() {
-  const windowWidth = window.innerWidth;
-  if(windowWidth < 1025) {
-    const numOfDots = document.querySelectorAll('.slides').length;
-    const carousel = document.querySelector('.slidesCarousel');
-    const carouselDots = document.createElement('div');
-    carouselDots.classList.add('dotsContainer');
-    for(let i=0; i<numOfDots;i++) {
-      const dot = document.createElement('div');
-      dot.classList.add('dot');
-      carouselDots.appendChild(dot).cloneNode(true);
-    }
-    carousel.appendChild(carouselDots).cloneNode(true);
-  }
-}
-
-function slideCarousel() {
-  let offset = 0;
-  let slides = document.querySelectorAll(".slides");
-  let carousel = document.querySelector(".slidesWrapper");
-  let nextButton = document.querySelector(".slidesNext");
-
-  let posInitial;
-  let slideWidth = slides[0].clientWidth;
-
-  // This is needed to make the carousel infinite. Clone the first 
-  // and last slide and then when you reach on both ends translate to the 
-  // corresponding slide
-  let firstSlide = slides[0].cloneNode(true);
-  let lastSlide = slides[slides.length - 1].cloneNode(true);
-  createCarouselDots();
-  const dots = document.querySelectorAll('.dot');
-  carousel.appendChild(firstSlide);
-  carousel.insertBefore(lastSlide, slides[0]);
-
-  nextButton.addEventListener("click", () => {
-      posInitial = carousel.offsetLeft;
-      carousel.style.left = `${posInitial - slideWidth}px `;
-      offset++;
-      carousel.classList.add('smooth');
-  });
-
-  let prevButton = document.querySelector(".slidesPrev");
-  prevButton.addEventListener("click", () => {
-      posInitial = carousel.offsetLeft;
-      carousel.style.left = `${posInitial + slideWidth}px`;
-      offset--;
-      carousel.classList.add('smooth');
-  });
-// if you are at the first slide wanting to go left 
-dots[0].classList.add('dotActive');
-  carousel.addEventListener('transitionend',()=>{
-    if(offset === -1) {
-      carousel.classList.remove('smooth');
-      carousel.style.left = `-${slides.length*slideWidth}px`;
-      offset = slides.length - 1;
-    }
-    // if you are at the last slide wanting to go right
-    if(offset === slides.length) {
-      carousel.classList.remove('smooth');
-      carousel.style.left = `-${slideWidth}px`;
-      offset = 0;
-    }
-    for(let i=0;i<dots.length; i++) {
-      dots[i].classList.remove('dotActive');
-    }
-    dots[offset].classList.add('dotActive');
-  });
-
-}
 function closeNavBar() {
   const windowWidth = window.innerWidth;
-  const closeBtn = document.querySelector('.closeBtn');
-  closeBtn.addEventListener('click',()=>{
-    const sideBar = document.querySelector('.sideBar');
-    sideBar.classList.remove('open');
-    if(windowWidth < 1024) {
-      document.querySelector('body').style.overflow = '';
+  const closeBtn = document.querySelector(".closeBtn");
+  closeBtn.addEventListener("click", () => {
+    const sideBar = document.querySelector(".sideBar");
+    sideBar.classList.remove("open");
+    if (windowWidth < 1024) {
+      document.querySelector("body").style.overflow = "";
     }
   });
 }
 function openNavBar() {
   const windowWidth = window.innerWidth;
-  const burgerBtn = document.querySelector('.sideBarBtn');
-  burgerBtn.addEventListener('click',()=>{
-    const sideBar = document.querySelector('.sideBar');
-    sideBar.classList.add('open');
-    if(windowWidth < 1024) {
-      document.querySelector('body').style.overflow = 'hidden';
+  const burgerBtn = document.querySelector(".sideBarBtn");
+  burgerBtn.addEventListener("click", () => {
+    const sideBar = document.querySelector(".sideBar");
+    sideBar.classList.add("open");
+    if (windowWidth < 1024) {
+      document.querySelector("body").style.overflow = "hidden";
     }
   });
 }
 
 function animatePersonalXp() {
-  if(window.innerWidth < 1025) {
-    const animationTarget = document.querySelectorAll('.workFieldsContent');
+  if (window.innerWidth < 1025) {
+    const animationTarget = document.querySelectorAll(".workFieldsContent");
     const options = {
-      threshold:0.9,
-      rootMargin: '-50px 0px'
-    }
-    const intersection = new IntersectionObserver((intersectionEntries)=>{
-      intersectionEntries.forEach((entry)=>{
-        if(entry.isIntersecting && !entry.target.classList.contains('waveAnimation')) {
-          entry.target.classList.add('waveAnimation');
+      threshold: 0.9,
+      rootMargin: "-50px 0px",
+    };
+    const intersection = new IntersectionObserver((intersectionEntries) => {
+      intersectionEntries.forEach((entry) => {
+        if (
+          entry.isIntersecting &&
+          !entry.target.classList.contains("waveAnimation")
+        ) {
+          entry.target.classList.add("waveAnimation");
         }
       });
-    },options);
-    animationTarget.forEach((target)=>{
+    }, options);
+    animationTarget.forEach((target) => {
       intersection.observe(target);
     });
   }
 }
-// TODO: 1) Add touchmove carousel,
+document.addEventListener("DOMContentLoaded", () => {
+  var slider = document.querySelector(".slidesCarousel"),
+    sliderItems = document.querySelector(".slidesWrapper"),
+    prev = document.querySelector(".slidesPrev"),
+    next = document.querySelector(".slidesNext");
+
+  function slide(wrapper, items, prev, next) {
+    var posX1 = 0,
+      posX2 = 0,
+      posInitial,
+      posFinal,
+      threshold = 100,
+      slides = items.querySelectorAll(".slides"),
+      slidesLength = slides.length,
+      slideSize = items.querySelectorAll(".slides")[0].offsetWidth,
+      firstSlide = slides[0],
+      lastSlide = slides[slidesLength - 1],
+      cloneFirst = firstSlide.cloneNode(true),
+      cloneLast = lastSlide.cloneNode(true),
+      index = 0,
+      allowShift = true;
+    createCarouselDots();
+    const dots = document.querySelectorAll(".dot");
+    items.appendChild(cloneFirst);
+    items.insertBefore(cloneLast, firstSlide);
+    items.addEventListener("touchstart", dragStart);
+    items.addEventListener("touchend", dragEnd);
+    items.addEventListener("touchmove", dragAction);
+    prev.addEventListener("click", function () {
+      shiftSlide(-1);
+    });
+    next.addEventListener("click", function () {
+      shiftSlide(1);
+    });
+    items.addEventListener("transitionend", checkIndex);
+
+    function dragStart(e) {
+      e = e || window.event;
+      e.preventDefault();
+      posInitial = items.offsetLeft;
+      posX1 = e.touches[0].clientX;
+    }
+
+    function dragAction(e) {
+      e = e || window.event;
+      posX2 = posX1 - e.touches[0].clientX;
+      posX1 = e.touches[0].clientX;
+      items.style.left = items.offsetLeft - posX2 + "px";
+    }
+
+    function dragEnd(e) {
+      posFinal = items.offsetLeft;
+      if (posFinal - posInitial < -threshold) {
+        shiftSlide(1, "drag");
+      } else if (posFinal - posInitial > threshold) {
+        shiftSlide(-1, "drag");
+      } else {
+        items.style.left = posInitial + "px";
+      }
+    }
+
+    function shiftSlide(dir, action) {
+      items.classList.add("smooth");
+
+      if (allowShift) {
+        if (!action) {
+          posInitial = items.offsetLeft;
+        }
+
+        if (dir == 1) {
+          items.style.left = posInitial - slideSize + "px";
+          index++;
+        } else if (dir == -1) {
+          items.style.left = posInitial + slideSize + "px";
+          index--;
+        }
+      }
+      allowShift = false;
+    }
+
+    function checkIndex() {
+      items.classList.remove("smooth");
+
+      if (index == -1) {
+        items.style.left = -(slidesLength * slideSize) + "px";
+        index = slidesLength - 1;
+      }
+
+      if (index == slidesLength) {
+        items.style.left = -(1 * slideSize) + "px";
+        index = 0;
+      }
+      allowShift = true;
+      if (window.innerWidth < 1025) {
+        for (let i = 0; i < dots.length; i++) {
+          dots[i].classList.remove("dotActive");
+        }
+        dots[index].classList.add("dotActive");
+      }
+    }
+  }
+
+  function createCarouselDots() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth < 1025) {
+      const numOfDots = document.querySelectorAll(".slides").length;
+      const carousel = document.querySelector(".slidesCarousel");
+      const carouselDots = document.createElement("div");
+      carouselDots.classList.add("dotsContainer");
+      for (let i = 0; i < numOfDots; i++) {
+        const dot = document.createElement("div");
+        dot.classList.add("dot");
+        carouselDots.appendChild(dot).cloneNode(true);
+      }
+      carousel.appendChild(carouselDots).cloneNode(true);
+      document.querySelector(".dot").classList.add("dotActive");
+    }
+  }
+  slide(slider, sliderItems, prev, next);
+});
